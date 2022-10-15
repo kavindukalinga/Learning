5d65d4af7cb00b19ab2ad70eade636bb9e957caa
kavindukalinga <74819742+kavindukalinga@users.noreply.github.com>
2022-10-15 11:06:36 +0530

    Add files via upload

---
+++ c/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("\t\t\t ___Welcome___ \n");
+    printf("\t\t\t\tProgrammed by: Kavindu Kalinga (UOM) \n");
+
+//    float g,f,c,x,y;
+//    printf("Enter the circle g,f,c: ");
+//    scanf("%f,%f,%f",&g,&f,&c);
+//
+//    if (g*g+f*f-c>0) {
+//        printf("Enter the point x,y: ");
+//        scanf("%f,%f",&x,&y);
+//        float s=x*x+y*y+2*g*x+2*f*y+c;
+//        if (s==0) printf("On the circle\n");
+//        else if (s>0) printf("Out of the circle");
+//        else printf("Inside the circle");
+//    }
+//    else printf("Invalid circle inputs\n");
+//
+//    for (int x=0; x<=127;x++) {
+//        printf("%d  %c\n",x,x); }
+
+
+
+
+
+    return 0;
+
+}
