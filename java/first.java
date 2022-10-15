ccb0754406b155c00e2860b7e3e3076e3430770c
kavindukalinga <74819742+kavindukalinga@users.noreply.github.com>
2022-10-15 11:02:18 +0530

    Add files via upload

---
+++ java/first.java
@@ -0,0 +1,23 @@
+import java.util.Scanner ;
+
+
+public class first
+{
+   public static void main(String args[])
+   {
+        System.out.println("Hello world! .....");
+        
+        Scanner sun = new Scanner(System.in);
+
+        System.out.println("....................");
+        
+        System.out.println(sun.nextLine());
+
+        
+          
+
+
+   }
+
+}
+
