b513ac305119ac06568f5aa3a15b811fa8c530a5
kavindukalinga <74819742+kavindukalinga@users.noreply.github.com>
2022-10-15 11:04:40 +0530

    Add files via upload

---
+++ cpp/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std; // This is standard library
+
+int main()
+{
+    /* This is the main function
+    buhahahahahaha */
+
+    int no1, no2, ans;
+
+    cout << "Enter first number "; cin >> no1;
+    cout << "Enter second number "; cin >> no2;
+    ans = no1 + no2;
+    cout << "The answer is " << ans<<endl;
+    cout << "hello " <<endl ;
+    return 0;
+}
